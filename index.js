bd396dc1e6c94939be8c9b82caaf627123e89bd0
Cayman Heng <hengcayman@gmail.com>
2020-11-30 18:21:28 -0500

    updated index.js to display licence badge

--- index.js
+++ index.js
@@ -48,56 +48,24 @@ const questions = [
     message: "Select a licence for your project",
     name: "licence",
     choices: [
-      "Academic Free License v3.0",
-      "Apache license 2.0",
-      "Artistic license 2.0",
-      "Boost Software License 1.0",
-      'BSD 2-clause "Simplified" license',
-      'BSD 3-clause "New" or "Revised" license',
-      "BSD 3-clause Clear license",
-      "Creative Commons license family",
-      "Creative Commons Zero v1.0 Universal",
-      "Creative Commons Attribution 4.0",
-      "Creative Commons Attribution Share Alike 4.0",
-      "Do What The F*ck You Want To Public License",
-      "Educational Community License v2.0",
-      "Eclipse Public License 1.0",
-      "Eclipse Public License 2.0",
-      "European Union Public License 1.1",
-      "GNU Affero General Public License v3.0",
-      "GNU General Public License family",
-      "GNU General Public License v2.0",
-      "GNU General Public License v3.0",
-      "GNU Lesser General Public License family",
-      "GNU Lesser General Public License v2.1",
-      "GNU Lesser General Public License v3.0",
+      "Apache",
+      "Academic",
+      "GNU",
       "ISC",
-      "LaTeX Project Public License v1.3c",
-      "Microsoft Public License",
       "MIT",
-      "Mozilla Public License 2.0",
-      "Open Software License 3.0",
-      "PostgreSQL License",
-      "SIL Open Font License 1.1",
-      "University of Illinois/NCSA Open Source License",
-      "The Unlicense",
-      "zLib License,",
+      "Mozilla",
+      "Open"
     ],
   },
 ];
-
-// function to write README file
-// const writeToFile = (fileName, data) => {
-//   fs.writeFile(fileName, JSON.stringify(data, null, "\t"), (err) =>
-//     err ? console.log(err) : console.log("Success!")
-//   );
-// };
 
 //function to create markdown
 const createMarkDown = (data) => {
   return `
     
     # ${data.title}
+
+    ![badge](https://img.shields.io/badge/license-${data.licence}-brightgreen)
 
     ## Table of Content
     - [Description](#description)
@@ -122,7 +90,7 @@ const createMarkDown = (data) => {
 
     ### Licence
 
-    ${data.licence}
+    This application is covered by the ${data.licence} licence.
 
     ### Contributing
 
@@ -154,6 +122,16 @@ const init = () => {
   });
 };
 
+// function call to initialize program
+init();
+
+// function to write README file
+// const writeToFile = (fileName, data) => {
+//   fs.writeFile(fileName, JSON.stringify(data, null, "\t"), (err) =>
+//     err ? console.log(err) : console.log("Success!")
+//   );
+// };
+
 // function to initialize program
 // const init = () => {
 //   inquirer.prompt(questions).then((data) => {
@@ -162,5 +140,47 @@ const init = () => {
 //   });
 // };
 
-// function call to initialize program
-init();
+// {
+//   type: "list",
+//   message: "Select a licence for your project",
+//   name: "licence",
+//   choices: [
+//     "Academic Free License v3.0",
+//     "Apache license 2.0",
+//     "Artistic license 2.0",
+//     "Boost Software License 1.0",
+//     'BSD 2-clause "Simplified" license',
+//     'BSD 3-clause "New" or "Revised" license',
+//     "BSD 3-clause Clear license",
+//     "Creative Commons license family",
+//     "Creative Commons Zero v1.0 Universal",
+//     "Creative Commons Attribution 4.0",
+//     "Creative Commons Attribution Share Alike 4.0",
+//     "Do What The F*ck You Want To Public License",
+//     "Educational Community License v2.0",
+//     "Eclipse Public License 1.0",
+//     "Eclipse Public License 2.0",
+//     "European Union Public License 1.1",
+//     "GNU Affero General Public License v3.0",
+//     "GNU General Public License family",
+//     "GNU General Public License v2.0",
+//     "GNU General Public License v3.0",
+//     "GNU Lesser General Public License family",
+//     "GNU Lesser General Public License v2.1",
+//     "GNU Lesser General Public License v3.0",
+//     "ISC",
+//     "LaTeX Project Public License v1.3c",
+//     "Microsoft Public License",
+//     "MIT",
+//     "Mozilla Public License 2.0",
+//     "Open Software License 3.0",
+//     "PostgreSQL License",
+//     "SIL Open Font License 1.1",
+//     "University of Illinois/NCSA Open Source License",
+//     "The Unlicense",
+//     "zLib License,",
+//   ],
+// },
+
+
+
